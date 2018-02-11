a998963a3279e3c484e718c33aa3a2d49b01ee23
idisfkj <idisfkj@gmail.com>
2018-02-11 15:30:46 +0800

    fix: 修复轮询线程异常

--- loopview/src/main/java/com/idisfkj/loopview/LoopView.java
+++ loopview/src/main/java/com/idisfkj/loopview/LoopView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
@@ -18,26 +19,31 @@ import android.widget.TextView;
 import com.idisfkj.loopview.adapter.LoopViewAdapter;
 import com.idisfkj.loopview.entity.LoopViewEntity;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by idisfkj on 16/8/8.
+ * @author idisfkj
+ * @date 16/8/8
  * Email : idisfkj@qq.com.
  */
-public class LoopView extends FrameLayout implements ViewPager.OnPageChangeListener, View.OnTouchListener {
+public class LoopView extends FrameLayout implements View.OnTouchListener {
     protected ViewPager viewPager;
-    protected LinearLayout linearCircler;
-    protected LinearLayout linearCirclerNo;
+    protected LinearLayout linearCircle;
+    protected LinearLayout linearCircleNo;
     protected LinearLayout linearLayout;
-    protected TextView descript;
+    protected TextView description;
     protected List<LoopViewEntity> list = new ArrayList<>();
     protected LoopViewAdapter adapter;
     protected int mCurrentPos;
-    protected ScheduledExecutorService mSes;
+    private ScheduledExecutorService mSes;
+    private ScheduledFuture<?> mScheduledFuture;
     protected OnItemClickListener listener;
     protected int rate;
     protected int bottomStyle;
@@ -45,6 +51,7 @@ public class LoopView extends FrameLayout implements ViewPager.OnPageChangeListe
     protected static final int DEF_BOTTOM_STYLE = 1;
     protected int defaultImageView;
     protected int errorImageView;
+    private InnerHandler mHandler = new InnerHandler(this);
 
     public LoopView(Context context) {
         this(context, null);
@@ -66,10 +73,25 @@ public class LoopView extends FrameLayout implements ViewPager.OnPageChangeListe
     public void init(Context context) {
         LayoutInflater.from(context).inflate(R.layout.loopview_layout, this, true);
         viewPager = (ViewPager) findViewById(R.id.loopView);
-        linearCircler = (LinearLayout) findViewById(R.id.linear_circler);
-        linearCirclerNo = (LinearLayout) findViewById(R.id.linear_circler_no);
-        descript = (TextView) findViewById(R.id.descript);
-        viewPager.addOnPageChangeListener(this);
+        linearCircle = (LinearLayout) findViewById(R.id.linear_circle);
+        linearCircleNo = (LinearLayout) findViewById(R.id.linear_circle_no);
+        description = (TextView) findViewById(R.id.description);
+        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+            @Override
+            public void onPageSelected(int position) {
+                mCurrentPos = position;
+                if (bottomStyle == getResources().getInteger(R.integer.loop_have_description)) {
+                    description.setText(list.get(position % list.size()).getDescript());
+                }
+                for (int i = 0; i < linearLayout.getChildCount(); i++) {
+                    if (i == position % list.size()) {
+                        linearLayout.getChildAt(i).setSelected(true);
+                    } else {
+                        linearLayout.getChildAt(i).setSelected(false);
+                    }
+                }
+            }
+        });
         viewPager.setOnTouchListener(this);
     }
 
@@ -83,20 +105,24 @@ public class LoopView extends FrameLayout implements ViewPager.OnPageChangeListe
 
     public void setLoopData(List<LoopViewEntity> loopData) {
         list = loopData;
+        linearCircle.removeAllViews();
+        linearCircleNo.removeAllViews();
+        mCurrentPos = 0;
+        shutdown();
         for (int i = 0; i < loopData.size(); i++) {
             ImageView imageView = new ImageView(getContext());
             imageView.setImageResource(R.drawable.loop_circler_bg);
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             params.leftMargin = 2;
             if (bottomStyle != DEF_BOTTOM_STYLE) {
-                descript.setVisibility(GONE);
+                description.setVisibility(GONE);
             }
-            if (bottomStyle == getResources().getInteger(R.integer.loop_no_descript_center)) {
-                linearCircler.setVisibility(GONE);
-                linearCirclerNo.setVisibility(VISIBLE);
-                linearLayout = linearCirclerNo;
+            if (bottomStyle == getResources().getInteger(R.integer.loop_no_description_center)) {
+                linearCircle.setVisibility(GONE);
+                linearCircleNo.setVisibility(VISIBLE);
+                linearLayout = linearCircleNo;
             } else {
-                linearLayout = linearCircler;
+                linearLayout = linearCircle;
             }
             imageView.setLayoutParams(params);
             linearLayout.addView(imageView);
@@ -110,37 +136,16 @@ public class LoopView extends FrameLayout implements ViewPager.OnPageChangeListe
         });
         viewPager.setAdapter(adapter);
         linearLayout.getChildAt(0).setSelected(true);
-        descript.setText(list.get(0).getDescript());
+        description.setText(list.get(0).getDescript());
 
-        if (defaultImageView != 0)
+        if (defaultImageView != 0) {
             adapter.setDefaultImageView(defaultImageView);
-        if (errorImageView != 0)
+        }
+        if (errorImageView != 0) {
             adapter.setErrorImageView(errorImageView);
+        }
         viewPager.setCurrentItem(list.size() * (Integer.MAX_VALUE / 1000));
         startLoop();
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        mCurrentPos = position;
-        if (bottomStyle == getResources().getInteger(R.integer.loop_have_descript))
-            descript.setText(list.get(position % list.size()).getDescript());
-        for (int i = 0; i < linearLayout.getChildCount(); i++) {
-            if (i == position % list.size())
-                linearLayout.getChildAt(i).setSelected(true);
-            else
-                linearLayout.getChildAt(i).setSelected(false);
-        }
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-
     }
 
     @Override
@@ -151,11 +156,21 @@ public class LoopView extends FrameLayout implements ViewPager.OnPageChangeListe
                 pauseLoop();
                 break;
             case MotionEvent.ACTION_UP:
-                if (mSes.isShutdown())
-                    startLoop();
+                startLoop();
                 break;
+            default:
+                return super.onTouchEvent(motionEvent);
         }
         return false;
+    }
+
+    protected void startLoop() {
+        mSes = createExecutor();
+        mScheduledFuture = mSes.scheduleAtFixedRate(new AutoRunnable(), rate, rate, TimeUnit.SECONDS);
+    }
+
+    protected void pauseLoop() {
+        mScheduledFuture.cancel(true);
     }
 
     protected class AutoRunnable implements Runnable {
@@ -169,24 +184,66 @@ public class LoopView extends FrameLayout implements ViewPager.OnPageChangeListe
         }
     }
 
-    protected Handler mHandler = new Handler() {
+
+    private static class InnerHandler extends Handler {
+        private WeakReference<LoopView> mTarget;
+
+        InnerHandler(LoopView loopView) {
+            mTarget = new WeakReference<>(loopView);
+        }
+
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            viewPager.setCurrentItem(mCurrentPos);
+            LoopView instance = mTarget.get();
+            if (instance != null) {
+                instance.viewPager.setCurrentItem(instance.mCurrentPos);
+            }
         }
-    };
-
-    protected void startLoop() {
-        mSes = Executors.newSingleThreadScheduledExecutor();
-        mSes.scheduleAtFixedRate(new AutoRunnable(), rate, rate, TimeUnit.SECONDS);
     }
 
-    protected void pauseLoop() {
-        mSes.shutdown();
+    private ScheduledExecutorService createExecutor() {
+        if (mSes == null || mSes.isShutdown()) {
+            mSes = new ScheduledThreadPoolExecutor(1, new ThreadFactory() {
+                int count;
+
+                @Override
+                public Thread newThread(@NonNull Runnable r) {
+                    final Thread thread = new Thread(r, "loopView-thread-" + count) {
+                        @Override
+                        public void run() {
+                            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND);
+                            super.run();
+                        }
+                    };
+                    count++;
+                    return thread;
+                }
+            });
+        }
+        return mSes;
+    }
+
+    private void shutdown() {
+        if (mSes != null && !mSes.isShutdown()) {
+            pauseLoop();
+            mSes.shutdown();
+            mSes = null;
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        shutdown();
     }
 
     public interface OnItemClickListener {
+        /**
+         * item click event
+         *
+         * @param position position of loopView
+         */
         void onItemClick(int position);
     }
 
